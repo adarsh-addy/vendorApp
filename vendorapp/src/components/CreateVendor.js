87c275c512a13989fd56f11ea87ea9bc14609e0c
adarsh-addy <adarsh80singh@gmail.com>
2022-10-18 22:40:33 +0530

    backend update

--- vendorapp/src/components/CreateVendor.js
+++ vendorapp/src/components/CreateVendor.js
@@ -1,71 +1,73 @@
 import React, { useEffect, useState } from "react";
-import '../styles/createvendor.css';
-import axios from 'axios';
-import { useNavigate} from "react-router-dom";
+import "../styles/createvendor.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateVendor() {
   const navigate = useNavigate();
-  let [cityProduct,SetCityProduct]=useState("");
-  let [countryProduct,SetCountryProduct]=useState("");
- let [vendorname,SetVendorname]=useState("");
- let [bank_acc_no,SetBank_acc_no]=useState("");
- let [bank_name,SetBank_name]=useState("");
- let [address,SetAddress]=useState("");
- let [city,SetCity]=useState("");
- let [country,SetCountry]=useState("");
- let [zip_code,SetZip_code]=useState("");
- console.log(vendorname);
- console.log(bank_acc_no);
- console.log(bank_name);
- console.log(address);
- console.log(city);
- console.log(country);
- console.log(zip_code);
+  let [cityProduct, SetCityProduct] = useState("");
+  let [countryProduct, SetCountryProduct] = useState("");
+  let [vendorname, SetVendorname] = useState("");
+  let [bank_acc_no, SetBank_acc_no] = useState("");
+  let [bank_name, SetBank_name] = useState("");
+  let [address, SetAddress] = useState("");
+  let [city, SetCity] = useState("");
+  let [country, SetCountry] = useState("");
+  let [zip_code, SetZip_code] = useState("");
+  console.log(vendorname);
+  console.log(bank_acc_no);
+  console.log(bank_name);
+  console.log(address);
+  console.log(city);
+  console.log(country);
+  console.log(zip_code);
 
- async function handleSave(e){
-e.preventDefault();
-const resp=await axios.post("http://localhost:4000/backend/save", {
-  vendorname,
-  bank_acc_no,
-  bank_name,
-  address,
-  city,
-  country,
-  zip_code
-}).catch((err)=>{
-  alert(err.response.data.message)
-})
-if(resp){
-  console.log(resp.data);
-  alert("Record inserted successfully");
-  navigate('/table')
-}
-SetVendorname("");
-SetBank_acc_no("");
-SetBank_name("");
-SetAddress("");
-SetCity("");
-SetCountry("");
-SetZip_code("");
- }
-
- async function handleCity() {
-  let result = await axios.get("http://localhost:4000/backend/showcity");
-  console.log(result.data.records);
-  SetCityProduct([...result.data.records]);
-}
-async function handleCountry() {
-  let result = await axios.get("http://localhost:4000/backend/showcountry");
-  console.log(result.data.records);
-  SetCountryProduct([...result.data.records]);
-}
-useEffect(()=>{
-  async function res() {
-    await handleCity()
-    await handleCountry()
+  async function handleSave(e) {
+    e.preventDefault();
+    const resp = await axios
+      .post("http://localhost:4000/backend/save", {
+        vendorname,
+        bank_acc_no,
+        bank_name,
+        address,
+        city,
+        country,
+        zip_code,
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+    if (resp) {
+      console.log(resp.data);
+      alert("Record inserted successfully");
+      navigate("/table");
+    }
+    SetVendorname("");
+    SetBank_acc_no("");
+    SetBank_name("");
+    SetAddress("");
+    SetCity("");
+    SetCountry("");
+    SetZip_code("");
   }
-  res();
-},[])
+
+  async function handleCity() {
+    let result = await axios.get("http://localhost:4000/backend/showcity");
+    console.log(result.data.records);
+    SetCityProduct([...result.data.records]);
+  }
+  async function handleCountry() {
+    let result = await axios.get("http://localhost:4000/backend/showcountry");
+    console.log(result.data.records);
+    SetCountryProduct([...result.data.records]);
+  }
+  useEffect(() => {
+    async function res() {
+      await handleCity();
+      await handleCountry();
+    }
+    res();
+  }, []);
   return (
     <div className="container">
       <form className="registration">
@@ -74,7 +76,12 @@ useEffect(()=>{
         <label>
           <span>Vendor name</span>
 
-          <input type="text" id="vendorname"  onChange={(e)=>SetVendorname(e.target.value)} required />
+          <input
+            type="text"
+            id="vendorname"
+            onChange={(e) => SetVendorname(e.target.value)}
+            required
+          />
 
           <ul className="input-requirements">
             <li>At least 3 characters long</li>
@@ -90,7 +97,7 @@ useEffect(()=>{
           <input
             type="number"
             id="number"
-            onChange={(e)=>SetBank_acc_no(e.target.value)}
+            onChange={(e) => SetBank_acc_no(e.target.value)}
             required
           />
 
@@ -106,54 +113,70 @@ useEffect(()=>{
         <label>
           <span>Bank name</span>
 
-          <input type="text" id="bankname" onChange={(e)=>SetBank_name(e.target.value)}  required />
-          </label>
+          <input
+            type="text"
+            id="bankname"
+            onChange={(e) => SetBank_name(e.target.value)}
+            required
+          />
+        </label>
 
         <label>
           <span>Address 1</span>
-          <textarea  id="address1" onChange={(e)=>SetAddress(e.target.value)}></textarea>
+          <textarea
+            id="address1"
+            onChange={(e) => SetAddress(e.target.value)}
+          ></textarea>
         </label>
         <label>
           <span>Address 2</span>
-          <textarea  id="address2"onChange={(e)=>SetAddress(e.target.value)}></textarea>
+          <textarea
+            id="address2"
+            onChange={(e) => SetAddress(e.target.value)}
+          ></textarea>
         </label>
         <label>
           <span>City</span>
 
-          <select id="city" onChange={(e)=>SetCity(e.target.value)}>
-          {cityProduct.length !== 0 ? (
-                            cityProduct.map((ele, idx) => {
-                              return <option key={idx}>{ele.city}</option>;
-                            })
-                          ) : (
-                            <option>No data</option>
-                          )}
-</select>
-          </label>
+          <select id="city" onChange={(e) => SetCity(e.target.value)}>
+            {cityProduct.length !== 0 ? (
+              cityProduct.map((ele, idx) => {
+                return <option key={idx}>{ele.city}</option>;
+              })
+            ) : (
+              <option>No data</option>
+            )}
+          </select>
+        </label>
 
-          <label>
+        <label>
           <span>Country</span>
 
-          <select id="country" onChange={(e)=>SetCountry(e.target.value)}>
-          {countryProduct.length !== 0 ? (
-                            countryProduct.map((ele, idx) => {
-                              return <option key={idx}>{ele.country}</option>;
-                            })
-                          ) : (
-                            <option>No data</option>
-                          )}
-</select>
-          </label>
+          <select id="country" onChange={(e) => SetCountry(e.target.value)}>
+            {countryProduct.length !== 0 ? (
+              countryProduct.map((ele, idx) => {
+                return <option key={idx}>{ele.country}</option>;
+              })
+            ) : (
+              <option>No data</option>
+            )}
+          </select>
+        </label>
 
-          <label>
+        <label>
           <span>Zip-code</span>
 
-          <input type="number" id="zipcode" onChange={(e)=>SetZip_code(e.target.value)} required />
-          </label>
+          <input
+            type="number"
+            id="zipcode"
+            onChange={(e) => SetZip_code(e.target.value)}
+            required
+          />
+        </label>
 
         <br />
 
-    <input type="submit"  onClick={handleSave} />
+        <input type="submit" onClick={handleSave} />
       </form>
     </div>
   );
--- vendorapp/src/components/VendorTable.js
+++ vendorapp/src/components/VendorTable.js
@@ -1,56 +1,60 @@
-import React,{useEffect, useState} from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 
 export default function VendorTable() {
-  let [product,setProduct]=useState("");
+  let [product, setProduct] = useState("");
   async function getData() {
     let result = await axios.get("http://localhost:4000/backend/show");
     // console.log(result.data.records);
     setProduct([...result.data.records]);
   }
-  useEffect(()=>{
-    async function res(){
+  useEffect(() => {
+    async function res() {
       await getData();
     }
     res();
-  },[])
+  }, []);
   return (
     <div className="container">
-    <div className='vendortable'>
+      <div className="vendortable">
         <h1> List of Vendors</h1>
         <Table>
-      <Thead>
-        <Tr>
-          <Th>S.No</Th>
-          <Th>Vendor Name</Th>
-          <Th>Bank Account No.</Th>
-          <Th>Bank Name</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-      {product.length !== 0 ? (
-                        product.map((ele, idx) => {
-                          return (
-                            <Tr key={idx}>
-                              <Td>{ele.id}</Td>
-                              <Td>{ele.vendorname}</Td>
-                              <Td>{ele.bank_acc_no}</Td>
-                              <Td>{ele.bank_name}</Td>
-                              <Td><button>Edit Vendor</button></Td>
-                              <Td><button>Delete Vendor</button></Td>
-                            </Tr>
-                          );
-                        })
-                      ) : (
-                        <Tr>
-                          <Td>NO data</Td>
-                        </Tr>
-                      )}
-      </Tbody>
-    </Table>
+          <Thead>
+            <Tr>
+              <Th>S.No</Th>
+              <Th>Vendor Name</Th>
+              <Th>Bank Account No.</Th>
+              <Th>Bank Name</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {product.length !== 0 ? (
+              product.map((ele, idx) => {
+                return (
+                  <Tr key={idx}>
+                    <Td>{ele.id}</Td>
+                    <Td>{ele.vendorname}</Td>
+                    <Td>{ele.bank_acc_no}</Td>
+                    <Td>{ele.bank_name}</Td>
+                    <Td>
+                      <button>Edit Vendor</button>
+                    </Td>
+                    <Td>
+                      <button>Delete Vendor</button>
+                    </Td>
+                  </Tr>
+                );
+              })
+            ) : (
+              <Tr>
+                <Td>NO data</Td>
+              </Tr>
+            )}
+          </Tbody>
+        </Table>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
