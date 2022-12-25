7c093c26b07d0a91eba8a0c81092ee0b7b290fbc
adarsh-addy <adarsh80singh@gmail.com>
2022-12-25 17:43:29 +0530

    first commit

--- vendorapp/src/App.js
+++ vendorapp/src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import {Routes,Route} from 'react-router-dom'
 import Vendor from './components/CreateVendor'
 import EditVendor from './components/EditVendor';
-import DeleteVenor from './components/DeleteVendor'
+// import DeleteVenor from './components/DeleteVendor'
 import VendorTable from './components/VendorTable';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Vendor/>} />
         <Route path='/editvendor' element={<EditVendor/>} />
-        <Route path='/deletevendor' element={<DeleteVenor/>} />
+        {/* <Route path='/deletevendor' element={<DeleteVendor/>} /> */}
         <Route path='/table' element={<VendorTable/>} />
       </Routes>
     </div>
